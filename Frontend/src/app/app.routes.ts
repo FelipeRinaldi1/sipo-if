9452811b6ded26a554eef30c305e692f984ee934
FelipeRinaldi1 <felipe.rinaldi.sobreira.dev0@gmail.com>
2026-08-09 19:04:30 -0300

    .

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardPublicoComponent } from './features/dashboard-publico/dashboard-publico';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardPublicoComponent },
+  { path: 'dashboard', component: DashboardPublicoComponent }
+];
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Register } from './features/user/register/register';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Register],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatTooltipModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
